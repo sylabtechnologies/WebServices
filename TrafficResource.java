2c26bac74fc79ce9c48d4d73b2a0c86bb7f95206
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2019-06-21 15:45:46 -0400

    init
    init

--- TrafficResource.java
+++ TrafficResource.java
@@ -1,7 +1,5 @@
 package com.mycompany.trafficservice;
 
-// of course change xml
-
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
@@ -21,6 +19,9 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 
@@ -33,11 +34,19 @@ import javax.ws.rs.container.Suspended;
 @Path("/")
 public class TrafficResource
 {
+    public static final Logger logger = Logger.getLogger(TrafficResource.class.getCanonicalName());
+
+    private Gson gson;
+    private static final Aircraft noData = new Aircraft(-1, null, null, null);
+
     @Context
     private UriInfo context;
-    @Context
-    private static Gson gson = new Gson();
-    private static final Aircraft noData = new Aircraft(-1, null, null, null);
+    
+    @PostConstruct
+    private void init()
+    {
+        gson = new Gson();
+    }
 
     @POST
     @Path("enqueue")
@@ -72,8 +81,7 @@ public class TrafficResource
         
         if (statusOK)
         {
-            Map<String, Object> properties = app.getProperties();
-            AicraftQueue q = (AicraftQueue) properties.get("QUEUE");
+            AicraftQueue q = getQueue(app);
             q.enqueue(a);
             return Response.status(Response.Status.OK).entity(a.getName() + " OK").build();
         }
@@ -87,12 +95,16 @@ public class TrafficResource
     @Produces(value = MediaType.APPLICATION_JSON)
     public String getJson(@Context Application app)
     {
-        Map<String, Object> properties = app.getProperties();
-        AicraftQueue q = (AicraftQueue) properties.get("QUEUE");
-        
+        AicraftQueue q = getQueue(app);
         Aircraft a = q.dequeue();
         if ( a == null) a = noData;
         return gson.toJson( a );
+    }
+
+    private AicraftQueue getQueue(Application app)
+    {
+        Map<String, Object> properties = app.getProperties();
+        return (AicraftQueue) properties.get("QUEUE");
     }
 
 }
